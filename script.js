70c0718412fcda156bec502b8e507d2bb3812c35
Cissa Machado <ceciliadsmachado@gmail.com>
2022-12-06 20:11:18 -0300

    feat: 🛠  add updates and new featur

--- script.js
+++ script.js
@@ -1,30 +1,60 @@
-// puxando os elementos do css
-let buttonReplay = document.querySelector("#replay");
-let score = document.querySelector("#score");
+// puxando os elementos canva do css
 let canvas = document.querySelector("#game");
 let ctx = canvas.getContext("2d");
-
-// criando as var da cobra do tabuleiro
-let snakeBoard = 64;
+ 
+let box= 32;
 let snake = [];
+
+// criando a da cobra
 snake[0] = {
-  x: 8 * snakeBoard,
-  y: 8 * snakeBoard,
+  x: 8 * box,
+  y: 8 * box,
 };
+
+let direction;
+
 // var dos quadradinhos
 let square = {
-  x: Math.floor(Math.random() * 15 + 1) * snakeBoard,
-  y: Math.floor(Math.random() * 15 + 1) * snakeBoard,
+  x: Math.floor(Math.random() * 15 + 1) * box,
+  y: Math.floor(Math.random() * 15 + 1) * box,
 };
 
-// criando a cobra
-function createSnake() {
-  for (let i = 0; i < snake.length; i++) {
-    ctx.fillStyle = "#FFFFFF";
-    ctx.fillRect = (snake[i].x, snake[i].y, box, box);
+// vars de outras funcinonalidas do jogo
+let score = 0;
+let level = 1;
+let vel = 150;
+let border = false;
+let fim = false;
+
+
+let btnScore = document.getElementById('points');
+let btnReplay = document.getElementById('replay');
+let btnLevel = document.getElementById('level');
+let btnBorder = document.getElementById('border');
+
+let spanLevel = document.getElementById('spanLevel');
+let spanBorder = document.getElementById('spanBorder');
+let spanScore = document.getElementById('score');
+
+let gameover = document.getElementById('gameover');
+
+// reinicia o jogo
+btnReplay.onclick = function(){
+  if(fim) {
+      window.location.reload();
   }
 }
-// criando os quadradinhos e *tentando* que eles gerem cores aleatorias*
-function createSquare(){
-
+// add uma borda, ou seja, a cobra só terá um detrminado espaço de atuação
+btnBorder.onclick = function(){    
+  if (!fim) {
+      if (!border) {
+          border = true;
+          canvas.classList.add('border');
+          spanBorder.innerHTML = 'Yes';
+      } else {
+          border = false;
+          canvas.classList.remove('border');
+          spanBorder.innerHTML = 'No';
+      }
+  }
 }
